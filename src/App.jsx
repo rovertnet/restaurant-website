492fd5ed817660015da6e006d8d7e489d5559085
rovertnet <matundukabamba@gmail.com>
2025-07-08 21:23:31 +0100

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Categories from "./body/composants/Categories.jsx";
 import ContactBanner from "./body/composants/ContactBanner.jsx";
 import FAQSection from "./body/composants/FAQSection.jsx";
 import FeaturesSection from "./body/composants/FeaturesSection.jsx";
+import Footer from "./body/composants/Footer.jsx";
 import MenuCards from "./body/composants/MenuCards.jsx";
 import PartnersSection from "./body/composants/PartnersSection.jsx";
 import PromoBanner from "./body/composants/PromoBanner.jsx";
@@ -26,6 +27,7 @@ function App() {
       <FAQSection />
       <PartnersSection />
       <ContactBanner />
+      <Footer />
     </>
   );
 }
