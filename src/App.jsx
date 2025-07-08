8a54fa3b741e15d7d49056b5fd523bbab4380832
rovertnet <matundukabamba@gmail.com>
2025-07-08 20:15:19 +0100

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Categories from "./body/composants/Categories.jsx";
 import FeaturesSection from "./body/composants/FeaturesSection.jsx";
 import MenuCards from "./body/composants/MenuCards.jsx";
+import PromoBanner from "./body/composants/PromoBanner.jsx";
 import TopPicks from "./body/composants/TopPicks.jsx";
 import HeroSection from "./body/HeroSection.jsx";
 import NavBar from "./head/NavBar.jsx"
@@ -16,6 +17,7 @@ function App() {
       <Categories />
       <TopPicks />
       <MenuCards />
+      <PromoBanner />
     </>
   );
 }
